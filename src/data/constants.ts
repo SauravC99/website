ee3038dd2cbab835a5fec97b516a2dca9a4b41aa
Saurav <sauravc41@gmail.com>
2023-06-24 23:55:26 -0700

    update footer links

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -13,6 +13,33 @@ export const MENU_LINKS = [
 	},
 ];
 
+export const MENU_LINKS_FOOTER = [
+	{
+		title: "Home",
+		path: "/",
+	},
+	{
+		title: "About",
+		path: "/about",
+	},
+	{
+		title: "Blog",
+		path: "/posts",
+	},
+	{
+		title: "LinkedIn",
+		path: "https://www.linkedin.com/in/saurav-chhapawala/",
+	},
+	{
+		title: "Github",
+		path: "https://github.com/SauravC99",
+	},
+	{
+		title: "Email",
+		path: "mailto:saurav.chhapawala@gmail.com",
+	},
+];
+
 // ! Remember to add your own socials
 export const SOCIAL_LINKS = {
 	github: "https://github.com/SauravC99",
