46b089579efec84393fc114000e05bf736ed1603
Saurav <sauravc41@gmail.com>
2023-11-15 04:18:29 -0800

    update site config for upcoming overhaul

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,31 +1,16 @@
-interface SiteConfig {
-	author: string;
-	title: string;
-	description: string;
-	lang: string;
-	ogLocale: string;
-	themeColorLight: string;
-	themeColorDark: string;
-	date: {
-		locale: string | string[] | undefined;
-		options: Intl.DateTimeFormatOptions;
-	};
-}
+import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: "Saurav Chhapawala",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
 	title: "Saurav",
-	// Meta property used as a default description meta property
+	// Meta property used as the default description meta property
 	description: "Welcome to my website",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-US",
 	// Meta property, found in src/components/BaseHead.astro L:42
 	ogLocale: "en_US",
-	// Sets the meta data theme-color, found in src/components/BaseHead.astro L:34. Toggling the dark mode will update the meta content with either light/dark color, implementation in src/layouts/Base.astro L:41.
-	themeColorLight: "#fafafa",
-	themeColorDark: "#1d1f21",
 	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
 	date: {
 		locale: "en-US",
@@ -35,4 +20,25 @@ export const siteConfig: SiteConfig = {
 			year: "numeric",
 		},
 	},
+	// Include view-transitions: https://docs.astro.build/en/guides/view-transitions/
+	includeViewTransitions: false,
+	webmentions: {
+		link: "",
+	},
 };
+
+// Used to generate links in both the Header & Footer.
+export const menuLinks: Array<{ title: string; path: string }> = [
+	{
+		title: "Home",
+		path: "/",
+	},
+	{
+		title: "About",
+		path: "/about/",
+	},
+	{
+		title: "Blog",
+		path: "/posts/",
+	},
+];
