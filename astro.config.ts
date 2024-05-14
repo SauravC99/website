1a4a6530dbcadceefdee6043f38db10db1bbab18
Saurav <sauravc41@gmail.com>
2024-05-14 01:21:12 -0700

    astro config

--- astro.config.ts
+++ astro.config.ts
@@ -1,36 +1,77 @@
 import { defineConfig } from "astro/config";
+import fs from "fs";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
-import prefetch from "@astrojs/prefetch";
+import remarkUnwrapImages from "remark-unwrap-images";
+import rehypeExternalLinks from "rehype-external-links";
+import { remarkReadingTime } from "./src/utils/remark-reading-time";
+import icon from "astro-icon";
+import expressiveCode from "astro-expressive-code";
+import { expressiveCodeOptions } from "./src/site.config";
 
 // https://astro.build/config
 export default defineConfig({
 	// ! Please remember to replace the following site property with your own domain
-	site: "https://astro-theme-cactus.netlify.app/",
+	site: "https://sauravch.com/",
 	markdown: {
-		shikiConfig: {
-			theme: "dracula",
-			wrap: true,
+		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+		rehypePlugins: [
+			[
+				rehypeExternalLinks,
+				{
+					target: "_blank",
+					rel: ["nofollow, noopener, noreferrer"],
+				},
+			],
+		],
+		remarkRehype: {
+			footnoteLabelProperties: {
+				className: [""],
+			},
 		},
 	},
 	integrations: [
-		mdx({}),
+		expressiveCode(expressiveCodeOptions),
+		icon(),
 		tailwind({
-			config: {
-				applyBaseStyles: false,
-			},
-		}),
-		image({
-			serviceEntryPoint: "@astrojs/image/sharp",
+			applyBaseStyles: false,
 		}),
 		sitemap(),
-		prefetch(),
+		mdx(),
 	],
+	image: {
+		domains: ["webmention.io"],
+		service: {
+			entrypoint: "astro/assets/services/sharp",
+			config: {
+				limitInputPixels: false,
+			},
+		},
+	},
+	// https://docs.astro.build/en/guides/prefetch/
+	prefetch: true,
 	vite: {
+		plugins: [rawFonts([".ttf", ".woff"])],
 		optimizeDeps: {
 			exclude: ["@resvg/resvg-js"],
 		},
 	},
 });
+
+function rawFonts(ext: Array<string>) {
+	return {
+		name: "vite-plugin-raw-fonts",
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore:next-line
+		transform(_, id) {
+			if (ext.some((e) => id.endsWith(e))) {
+				const buffer = fs.readFileSync(id);
+				return {
+					code: `export default ${JSON.stringify(buffer)}`,
+					map: null,
+				};
+			}
+		},
+	};
+}
